21c432894002633a04aa3ad9e11106d5674ce244
keyzen <alonso.anavalon@gmail.com>
2021-09-05 17:50:44 -0300

    fix rutas views

--- src/app.js
+++ src/app.js
@@ -8,15 +8,19 @@ const exphbs = require('express-handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const path = require('path')
 
+
 const hbs = exphbs.create({
     handlebars: allowInsecurePrototypeAccess(Handlebars), 
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
     defaultLayout: 'main',
     extname: '.hbs'
 })
 
 // Settings
+
+
+
 app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
