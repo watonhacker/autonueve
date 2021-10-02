143605d2f386a97b7337fa05a63db95dcb0a6848
keyzen <alonso.anavalon@gmail.com>
2021-10-02 20:53:20 -0300

    frontend2

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,6 @@
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
-console.log(process.env.HOST)
-console.log(process.env.USER)
-console.log(process.env.PASS)
-console.log(process.env.DATABASE)
-
 const mysql = require('mysql');
 
 
