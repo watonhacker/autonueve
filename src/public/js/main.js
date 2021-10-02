029ee33e86c11f3321d520c0f329329fdb372577
keyzen <alonso.anavalon@gmail.com>
2021-10-02 20:50:05 -0300

    frontend

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,6 +5,8 @@ import { getSubmodels } from "/js/spareSearcher/getSubmodels.js"
 import { printSubmodels } from "/js/spareSearcher/printSubmodels.js"
 import { printYears } from "/js/spareSearcher/printYears.js"
 import { searchBtn } from "./spareSearcher/searchBtn.js"
+import hamburgerMenu from "./hamburgerBtn.js";
+import responsiveQueries from "./responsive.js";
 
 d.addEventListener("DOMContentLoaded", () => {
 
@@ -13,6 +15,8 @@ d.addEventListener("DOMContentLoaded", () => {
     printYears()
     printModels()
     searchBtn()
+    hamburgerMenu(".nav-btn", ".nav-menu", ".nav-menu a", "active");
+    responsiveQueries("btn-menu", "(min-width: 768px)", "hidden");
 
 
 
