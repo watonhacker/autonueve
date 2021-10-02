a6c087dfb8d81305ae0a0108a5abe8c80fe7d448
keyzen <alonso.anavalon@gmail.com>
2021-10-02 01:06:54 -0300

    i want to cry

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 console.log(process.env.HOST)
 console.log(process.env.USER)
-console.log(process.env.PASSWORD)
+console.log(process.env.PASS)
 console.log(process.env.DATABASE)
 
 const mysql = require('mysql');
@@ -12,7 +12,7 @@ const mysql = require('mysql');
     mysqlConnection = mysql.createConnection({
     host:process.env.HOST,
     user:process.env.USER,
-    password:process.env.PASSWORD,
+    password:process.env.PASS,
     database:process.env.DATABASE
     })
 
