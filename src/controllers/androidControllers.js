158d92d0fcb1596fbef49afe56ff2c5f204cd18d
keyzen <alonso.anavalon@gmail.com>
2021-12-02 10:55:59 -0300

    probandoAndroid

--- src/controllers/androidControllers.js
+++ src/controllers/androidControllers.js
@@ -6,9 +6,9 @@ exports.getBrands = function () {
         return new Promise((resolve, reject) => {
             mysqlConnection.query("SELECT * FROM producto LIMIT 5", (err, results) => {
                 results = JSON.parse(JSON.stringify(results))
-                resolve({
+                resolve(
                     results
-                })
+                )
                 
             } )
 
