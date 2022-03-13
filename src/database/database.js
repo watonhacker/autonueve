7bcbda8553df188a97980544d0ce2d9d10e230cd
k3yzen <alonso.anavalon@gmail.com>
2022-03-13 12:05:42 -0300

    cambiando bdd

--- src/database/database.js
+++ src/database/database.js
@@ -8,20 +8,29 @@ console.log(process.env.DATABASE)
 console.log("***") */
 
 const mysql = require('mysql');
+const host = 'localhost' /* || 'mobalzen-autonueve-do-user-9924163-0.b.db.ondigitalocean.com' */
 
-    mysqlConnection = mysql.createConnection({
-    host:'mobalzen-autonueve-do-user-9924163-0.b.db.ondigitalocean.com',
+/*     mysqlConnection = mysql.createConnection({
+    host,
     user:"doadmin",
     password:"lNIFw7eUbjhpbisK",
     database:"autonueve",
     port:'25060'
     })
+ */
+    mysqlConnection = mysql.createConnection({
+        host,
+        user:"root",
+        password:proccess.env.PASSWORD,
+        database:"autonueve",
+        port:'3306'
+        })
 
 
 
     mysqlConnection.connect((err) => {
         if (err) throw err;
-        console.log("Conectado a la base de datos ", process.env.DATABASE)
+        console.log("Conectado a la base de datos ", host)
     })
 
 
