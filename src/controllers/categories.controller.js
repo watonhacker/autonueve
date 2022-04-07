7bb1fc17474db6ee7b73cfe79c7b1aed2c630ecc
k3yzen <alonso.anavalon@gmail.com>
2022-04-07 00:03:10 -0400

    [BusquedaCategoria] - [FEAT] - Menu function ok

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -22,3 +22,21 @@ exports.getCategories = () => {
 
 
 };
+
+
+exports.getCategoryProducts = (categoryId) => {
+    let sql = `SELECT * FROM producto WHERE producto.categoria_id = ${categoryId}`;
+
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query(sql, (err, results, rows) => {
+            results=JSON.parse(JSON.stringify(results))
+            resolve(results)
+
+            if (err) {
+                console.error(err);
+                return [];
+            }
+            
+        })
+    })
+}
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const categoriesControllers = require('../controllers/categories.controller')
-const neumaticosController = require('../controllers/neumaticos.controller')
+
 
 router.get('/', async (req, res) => {
     let categories = await categoriesControllers.getCategories();
@@ -8,7 +8,35 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/neumaticos', async (req, res) => {
-    let resultados = await neumaticosController.getNeumaticos();
+    // category 1 = neumaticos
+    let resultados = await categoriesControllers.getCategoryProducts(1);
+    console.log(resultados)
+    res.render("category-search", {
+        resultados
+    })
+})
+
+router.get('/aceite', async (req, res) => {
+    // category 1 = neumaticos
+    let resultados = await categoriesControllers.getCategoryProducts(2);
+    console.log(resultados)
+    res.render("category-search", {
+        resultados
+    })
+})
+
+router.get('/filtro', async (req, res) => {
+    // category 1 = neumaticos
+    let resultados = await categoriesControllers.getCategoryProducts(3);
+    console.log(resultados)
+    res.render("category-search", {
+        resultados
+    })
+})
+
+router.get('/bateria', async (req, res) => {
+    // category 1 = neumaticos
+    let resultados = await categoriesControllers.getCategoryProducts(4);
     console.log(resultados)
     res.render("category-search", {
         resultados
