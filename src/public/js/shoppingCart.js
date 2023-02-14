db749858b3586ff0172dcb1496a66ba1df4bf2f9
k3yzen <alonso.anavalon@gmail.com>
2023-02-14 20:48:02 -0300

    cambios varios sobre todo de carrito

--- src/public/js/shoppingCart.js
+++ src/public/js/shoppingCart.js
@@ -1,5 +1,6 @@
 ((function(){
 
+    debugger;
 
     let shoppingCart = document.querySelector(".shopping-cart") 
 
@@ -81,7 +82,7 @@
 
  
     
-
+    debugger;
     document.querySelectorAll("[href='/checkout']")[0].addEventListener("click", e => {
         e.preventDefault()
         url = "/checkout"
@@ -213,7 +214,282 @@
         }
 
     })
+    document.querySelectorAll("[href='/checkout']")[2].addEventListener("click", e => {
+        e.preventDefault()
+        url = "/checkout"
 
+        let product_list = myStorage.getItem("listaproducto")
+        
+        if (product_list == undefined ){ 
+            Swal.fire("No tiene productos agregados a su carrito")
+        } else {
+
+            try {
+                product_list = product_list.split(",")
+
+                let objeto = {}
+       
+        
+                let primerNumero ="";
+                let segundoNumero ="";
+        
+        
+                product_list.forEach((par) => {
+                    primerNumero = ""
+                    segundoNumero = ""
+                    for (element of par) {
+                        
+                        if (element != ":") {
+                            primerNumero += element
+                        } else {
+                            break
+                        }
+                    }
+                    
+                    let newString = "";
+                    for (let i = par.length - 1; i >= 0; i--) {
+                        newString += par[i];
+                    }
+                
+                
+                    for (element of newString) {
+                        if (element != ":") {
+                            segundoNumero += element
+                        } else {
+        
+                            break
+                        }
+                    }
+    
+                    objeto[primerNumero] = segundoNumero.split("").reverse().join("")
+                
+        
+                })
+                
+      
+    
+                const selectedProducts = Object.keys(objeto);
+                const selectedAmounts = Object.values(objeto);
+            
+                debugger;
+                window.location.href = `/checkout?items=[${selectedProducts}]&amounts=[${selectedAmounts}]`
+
+    
+            
+            } catch(err) {
+                console.log(err)
+            }
+            
+        }
+
+    })
+    document.querySelectorAll("[href='/checkout']")[3].addEventListener("click", e => {
+        e.preventDefault()
+        url = "/checkout"
+
+        let product_list = myStorage.getItem("listaproducto")
+        
+        if (product_list == undefined ){ 
+            Swal.fire("No tiene productos agregados a su carrito")
+        } else {
+
+            try {
+                product_list = product_list.split(",")
+
+                let objeto = {}
+       
+        
+                let primerNumero ="";
+                let segundoNumero ="";
+        
+        
+                product_list.forEach((par) => {
+                    primerNumero = ""
+                    segundoNumero = ""
+                    for (element of par) {
+                        
+                        if (element != ":") {
+                            primerNumero += element
+                        } else {
+                            break
+                        }
+                    }
+                    
+                    let newString = "";
+                    for (let i = par.length - 1; i >= 0; i--) {
+                        newString += par[i];
+                    }
+                
+                
+                    for (element of newString) {
+                        if (element != ":") {
+                            segundoNumero += element
+                        } else {
+        
+                            break
+                        }
+                    }
+    
+                    objeto[primerNumero] = segundoNumero.split("").reverse().join("")
+                
+        
+                })
+                
+      
+    
+                const selectedProducts = Object.keys(objeto);
+                const selectedAmounts = Object.values(objeto);
+            
+                debugger;
+                window.location.href = `/checkout?items=[${selectedProducts}]&amounts=[${selectedAmounts}]`
+
+    
+            
+            } catch(err) {
+                console.log(err)
+            }
+            
+        }
+
+    })
+    document.querySelectorAll("[href='/checkout']")[4].addEventListener("click", e => {
+        e.preventDefault()
+        url = "/checkout"
+
+        let product_list = myStorage.getItem("listaproducto")
+        
+        if (product_list == undefined ){ 
+            Swal.fire("No tiene productos agregados a su carrito")
+        } else {
+
+            try {
+                product_list = product_list.split(",")
+
+                let objeto = {}
+       
+        
+                let primerNumero ="";
+                let segundoNumero ="";
+        
+        
+                product_list.forEach((par) => {
+                    primerNumero = ""
+                    segundoNumero = ""
+                    for (element of par) {
+                        
+                        if (element != ":") {
+                            primerNumero += element
+                        } else {
+                            break
+                        }
+                    }
+                    
+                    let newString = "";
+                    for (let i = par.length - 1; i >= 0; i--) {
+                        newString += par[i];
+                    }
+                
+                
+                    for (element of newString) {
+                        if (element != ":") {
+                            segundoNumero += element
+                        } else {
+        
+                            break
+                        }
+                    }
+    
+                    objeto[primerNumero] = segundoNumero.split("").reverse().join("")
+                
+        
+                })
+                
+      
+    
+                const selectedProducts = Object.keys(objeto);
+                const selectedAmounts = Object.values(objeto);
+            
+                debugger;
+                window.location.href = `/checkout?items=[${selectedProducts}]&amounts=[${selectedAmounts}]`
+
+    
+            
+            } catch(err) {
+                console.log(err)
+            }
+            
+        }
+
+    })
+    document.querySelectorAll("[href='/checkout']")[5].addEventListener("click", e => {
+        e.preventDefault()
+        url = "/checkout"
+
+        let product_list = myStorage.getItem("listaproducto")
+        
+        if (product_list == undefined ){ 
+            Swal.fire("No tiene productos agregados a su carrito")
+        } else {
+
+            try {
+                product_list = product_list.split(",")
+
+                let objeto = {}
+       
+        
+                let primerNumero ="";
+                let segundoNumero ="";
+        
+        
+                product_list.forEach((par) => {
+                    primerNumero = ""
+                    segundoNumero = ""
+                    for (element of par) {
+                        
+                        if (element != ":") {
+                            primerNumero += element
+                        } else {
+                            break
+                        }
+                    }
+                    
+                    let newString = "";
+                    for (let i = par.length - 1; i >= 0; i--) {
+                        newString += par[i];
+                    }
+                
+                
+                    for (element of newString) {
+                        if (element != ":") {
+                            segundoNumero += element
+                        } else {
+        
+                            break
+                        }
+                    }
+    
+                    objeto[primerNumero] = segundoNumero.split("").reverse().join("")
+                
+        
+                })
+                
+      
+    
+                const selectedProducts = Object.keys(objeto);
+                const selectedAmounts = Object.values(objeto);
+            
+                debugger;
+                window.location.href = `/checkout?items=[${selectedProducts}]&amounts=[${selectedAmounts}]`
+
+    
+            
+            } catch(err) {
+                console.log(err)
+            }
+            
+        }
+
+    })
 
     
     
